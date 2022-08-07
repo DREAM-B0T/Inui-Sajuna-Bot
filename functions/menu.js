ecb68d2d4190b053326a2754f5f09788cc4d38c5
DREAM-B0T <95890009+DREAM-B0T@users.noreply.github.com>
2022-08-07 12:58:09 -0500

    Add files via upload

---
+++ functions/menu.js
@@ -0,0 +1,291 @@
+exports.menu = (v) => {
+  return `
+  
+┌─⊷ OTROS MENUS. . .
+
+Si eres Admin: .admin
+Si eres Premium: .premium
+Si eres Owner: .omenu
+
+ ───── ❝ *MAIN* ❞ ─────
+ 
+╭─˚ ༘✶ ⋆｡ ⁀➷─────────
+│
+│  .reglas
+│  .reglasrol
+│  .reglasbt
+│
+│ ┌━━━
+│ │ Ve las distintas
+│ ▢ reglas del grupo,
+│ │ bt = battle.
+│ └━━━
+╰──────────────╮
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│  .profile
+│
+│ ┌━━━
+│ ▢ Ve tu perfil.
+│ └━━━
+╰──────────────╮
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│  .link
+│
+│ ┌━━━
+│ ▢ Link del grupo.
+│ └━━━
+╰──────────────╮
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│  .warns / .profile
+│
+│ ┌━━━
+│ ▢ Ve tus advertencias.
+│ └━━━
+╰──────────────╮
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│  .infogp
+│
+│ ┌━━━
+│ │ Ve la info de el
+│ │ grupo.
+│ └━━━
+╰──────────────
+
+  ───── ❝ *OTROS* ❞ ─────
+
+╭─˚ ༘✶ ⋆｡ ⁀➷─────────
+│
+│  .bot (texto)
+│
+│ ┌━━━
+│ │ Puedes hablar con el
+│ ▢ BOT y tambien enseñarle
+│ │ a hablar.
+│ └━━━
+╰──────────────╮
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│  .info
+│
+│ ┌━━━
+│ ▢ Ve la info del BOT.
+│ └━━━
+╰──────────────╮
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│  .ping
+│
+│ ┌━━━
+│ ▢ Ping del Bot.
+│ └━━━
+╰──────────────╮
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│  .owner
+│
+│ ┌━━━
+│ ▢ Contacto de mi creador.
+│ └━━━
+╰──────────────╮
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│  .crear
+│
+│ ┌━━━
+│ │ Descubre como hacer tu
+│ │ propio BOT
+│ └━━━
+╰──────────────
+
+  ───── ❝ *FUN* ❞ ─────
+  
+╭──˚ ༘✶ ⋆｡ ⁀➷───────
+│
+│   .fake text|@user|text
+│
+│ ┌━━━
+│ ▢ Creo un texto falso
+│ │ respondiendo a un
+│ │ mensaje falso de un
+│ ▢ integrante.
+│ └━━━
+╰──────────────╮
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│  .preg (texto)
+│
+│ ┌━━━
+│ ▢ Respondo una pregunta.
+│ └━━━
+╰──────────────
+
+  ───── ❝ *GAMES* ❞ ─────
+
+╭──˚ ༘✶ ⋆｡ ⁀➷───────
+│
+│  .verdad
+│
+│ ┌━━━
+│ ▢ Te mando una verdad.
+│ └━━━
+╰──────────────╮
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│  .reto
+│
+│ ┌━━━
+│ ▢ Te mando un reto.
+│ └━━━
+╰──────────────╮
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│  .ppt
+│
+│ ┌━━━
+│ │ Jugamos piedra, papel
+│ │ o tijera.
+│ └━━━
+╰──────────────
+
+  ────❝ *STICKER* ❞────
+
+╭──˚ ༘✶ ⋆｡ ⁀➷───────
+│
+│  .s / .sticker
+│
+│ ┌━━━
+│ ▢ Crea un sticker
+│ │ etiquetando una imagen
+│ │ o un video que dure
+│ ▢ menos de 10sec.
+│ └━━━
+╰──────────────╮
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│  .toimg
+│  
+│ ┌━━━
+│ │ Convierte un sticker
+│ │ a una imagen.
+│ └━━━
+╰──────────────╮
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│
+│  .take nombre|autor
+│
+│ ┌━━━
+│ ▢ Roba un sticker.
+│ └━━━
+╰──────────────
+
+  ───── ❝ *TOOLS* ❞ ─────
+
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│  .afk (Razón)
+│  
+│ ┌━━━
+│ │ Estaras AFK hasta que
+│ │ envies un mensaje.
+│ └━━━ 
+╰──────────────╮
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│  .tomp3
+│
+│ ┌━━━
+│ │ Convierte un video a
+│ │ un audio.
+│ └━━━ 
+╰──────────────╮
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│  .toav
+│
+│ ┌━━━
+│ │ Convierte un audio a
+│ │ video.
+│ └━━━ 
+╰──────────────╮
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│  .say (texto)
+│  
+│ ┌━━━
+│ ▢ Repito lo que dijiste.
+│ └━━━ 
+╰──────────────
+
+  ────❝ *SEARCH* ❞────
+
+╭──˚ ༘✶ ⋆｡ ⁀➷───────
+│
+│  .ytsearch (Busqueda)
+│
+│ ┌━━━ 
+│ │ Busco lo que me pidas 
+│ │ en YouTube.
+│ └━━━ 
+╰──────────────╮
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│  .google (Busqueda)
+│ ┌━━━
+│ │ Busco lo que me pidas
+│ │ en Google.
+│ └━━━ 
+╰──────────────╮
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│  .playstore
+│  
+│ ┌━━━
+│ │ Busco lo que me pidas
+│ │ en PlayStore.
+│ └━━━ 
+╰──────────────
+
+ ─── ❝ *DOWNLOAD* ❞ ───
+
+╭──˚ ༘✶ ⋆｡ ⁀➷───────
+│
+│  TODOS ESTOS COMANDOS
+│  SIRVEN PARA DESCARGAS.
+│ ┌━━━
+│ │ .tiktok (link)
+│ │ .igdl (link)
+│ │ .ytmp3 (link)
+│ │ .ytmp4 (link)
+│ │ .tiktokaudio (link)
+│ └━━━ 
+╰──────────────╮
+╭──˚ ༘✶ ⋆｡ ⁀➷───────╯
+│
+│ ⚠️EN MANTEMINIENTO!!
+│ NO LOS USES.⚠️
+│
+│  .play
+│  .play2
+│  .playvid
+│
+│ ┌━━━
+│ ▢ Reproduce videos.
+│ └━━━
+╰──────────────`;
+};
+
+exports.menuVC = `
+┌─⊷ *MAIN* 
+▢ menu
+└─────────────
+┌─⊷ *BUSQUEDA*
+▢ google
+└─────────────
+┌─⊷ *DOWNLOAD* 
+▢ play
+└─────────────`;
