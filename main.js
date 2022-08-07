b01c79871ee84b352a080243ccde211b6145f573
DREAM-B0T <95890009+DREAM-B0T@users.noreply.github.com>
2022-08-07 11:31:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-
+const { WAConnection: _WAConnection, ReconnectMode, MessageType, MessageOptions } = require('@adiwajshing/baileys');
+const simple = require("./whatsapp/connecting.js");
+const WAConnection = simple.WAConnection(_WAConnection);
+const verity = new WAConnection();
+const {
+  cekWelcome,
+  cekAntilink,
+  cekBadword,
+  cekAntidelete,
+  cekDetect
+} = require('./functions/group');
